6b049dfa8ceea9e6e1b77d27d0f7897338d08a2c
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-18 17:35:39 +0100

    done

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,3 +1,4 @@
+
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Tools from "./components/Tools"
--- landing-page/src/constants/index.js
+++ landing-page/src/constants/index.js
@@ -2,31 +2,37 @@ export const NavLinks = [
     {
         id: "1",
         title: "Home",
-        icon: "Home"
+        icon: "Home",
+        path: "/"
     },
     {
         id: "2",
         title: "Projects",
-        icon: "File"
+        icon: "File",
+        path: "/projects"
     },
     {
         id: "3",
         title: "Tools",
-        icon: "Hammer"
+        icon: "Hammer",
+        path: "/tools"
     },
     {
         id: "4",
         title: "Experiences",
-        icon: "Award"
+        icon: "Award",
+        path: "/experiences"
     },
     {
         id: "5",
         title: "Thoughts",
-        icon: "Zap"
+        icon: "Zap",
+        path: "/thoughts"
     },
     {
         id: "6",
         title: "Contacts",
-        icon: "Mail"
+        icon: "Mail",
+        path: "/contact"
     }
 ]
