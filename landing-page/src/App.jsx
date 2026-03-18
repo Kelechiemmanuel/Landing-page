6f30c754cca761129f38e6ec90cf64b3d9dd9a00
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-18 17:48:30 +0100

    done

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,18 +1,30 @@
 
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Tools from "./components/Tools"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Tools from "./components/Tools";
+import Experience from "./components/Experience";
+import Thoughts from "./components/Thoughts";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className="bg-[#131415]">
-      <div>
+    <BrowserRouter>
+      <div className="bg-[#131415] min-h-screen overflow-x-hidden">
         <Navbar />
-        <Hero />
-      </div>
-    </div>
-    
-  )
-}
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/tool" element={<Tools />} />
+          <Route path="/experiences" element={<Experience />} />
+          <Route path="/thoughts" element={<Thoughts />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
 
-export default App
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- landing-page/src/components/Navbar.jsx
+++ landing-page/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Home, File, Hammer, Award, Zap, Mail } from "lucide-react";
 import { useState } from "react";
 import { NavLinks } from "../constants";
+import { Link } from "react-router-dom"
 
 const icons = {
     Home: Home,
@@ -21,13 +22,13 @@ const Navbar = () => {
                     const Icon = icons[nav.icon];
                     return (
                         <li key={nav.id} className="flex justify-center items-center flex-col relative group">
-                            <a href="#" className={`text-white rounded-sm p-1
+                            <Link to={nav.path} className={`text-white rounded-sm p-1
                                 ${active === nav.title ? "bg-amber-500" : "hover:bg-amber-500"}
                             `}
                               onClick={() => setActive(nav.title)}
                             > 
                                 <Icon className="w-5 h-5 md:w-6 md:h-6" />
-                            </a>
+                            </Link>
                             <span className="text-sm absolute top-13 hidden group-hover:block text-white">{nav.title}</span>
                         </li>
                     );
