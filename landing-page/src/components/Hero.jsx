0306692d2663f394d0bcadd871521be547bb0967
kelechiemmanuel <ekelechi413@gmail.com>
2026-03-20 17:13:54 +0100

    done

--- landing-page/src/components/Hero.jsx
+++ landing-page/src/components/Hero.jsx
@@ -9,11 +9,12 @@ import Spinner from "./Spinner"
 import { useEffect, useState } from "react"
 
 const Hero = ({ activeComponent }) => {
+    const location = useLocation();
+    const isHome = location.pathname === "/";
     const getHighlightClass = (componentName) =>
         activeComponent === componentName ? "border-4 border-amber-500 rounded-xl p-3" : "";
 
     const [loading, setLoading] = useState(true);
-    const location = useLocation();
 
     useEffect(() => {
 
@@ -36,35 +37,37 @@ const Hero = ({ activeComponent }) => {
             lg:flex-row
             md:px-30
             ">
-                <div className="flex flex-col justify-center items-center py-10 px-20 my-10 bg-[#292a2b] text-center rounded-2xl top-14 h-full 
+                {isHome && (
+                    <div className="flex flex-col justify-center items-center py-10 px-20 my-10 bg-[#292a2b] text-center rounded-2xl top-14 h-full
                 md:px-20 
                 lg:px-7 lg:sticky
                 ">
-                    <div className="flex shrink-0 justify-center items-center">
-                        <img src={portfolio} alt="profile-picture" className="w-60 h-70 object-top rounded-xl shrink-0" />
+                        <div className="flex shrink-0 justify-center items-center">
+                            <img src={portfolio} alt="profile-picture" className="w-60 h-70 object-top rounded-xl shrink-0" />
+                        </div>
+                        <div className="mt-10">
+                            <h1 className="font-bold text-2xl lg:text-3xl text-[#f6f6f6]">Joshua Emmanuel</h1>
+                            <p className="text-[#f6f6f6] text-sm">Product Designer & Developer <br /> Lagos, Nigeria</p>
+                        </div>
+                        <div className="flex gap-5 justify-center items-center my-5">
+                            <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
+                                <Mail className="w-6 h-6" />
+                            </button>
+                            <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
+                                <Dribbble className="w-6 h-6" />
+                            </button>
+                            <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
+                                <Twitter className="w-6 h-6" />
+                            </button>
+                            <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
+                                <Instagram className="w-6 h-6" />
+                            </button>
+                        </div>
+                        <div className="mt-10">
+                            <Talk />
+                        </div>
                     </div>
-                    <div className="mt-10">
-                        <h1 className="font-bold text-2xl lg:text-3xl text-[#f6f6f6]">Joshua Emmanuel</h1>
-                        <p className="text-[#f6f6f6] text-sm">Product Designer & Developer <br /> Lagos, Nigeria</p>
-                    </div>
-                    <div className="flex gap-5 justify-center items-center my-5">
-                        <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
-                            <Mail className="w-6 h-6" />
-                        </button>
-                        <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
-                            <Dribbble className="w-6 h-6" />
-                        </button>
-                        <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
-                            <Twitter className="w-6 h-6" />
-                        </button>
-                        <button className="text-[#f6f6f6] bg-none hover:bg-amber-500 p-1 rounded-sm cursor-pointer outline-0">
-                            <Instagram className="w-6 h-6" />
-                        </button>
-                    </div>
-                    <div className="mt-10">
-                        <Talk />
-                    </div>
-                </div>
+                )}
 
                 <motion.div className="py-10 h-full w-full md:w-full lg:w-[45%]"
                 >
@@ -81,7 +84,9 @@ const Hero = ({ activeComponent }) => {
                                 damping: 14
                             }}
                         >
-                            <Outlet />
+                            <div>
+                                <Outlet />
+                            </div>
 
                             <div className={getHighlightClass("Question")}>
                                 <Question />
